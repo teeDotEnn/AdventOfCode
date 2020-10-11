8d194b83deeb2dd08fae1c8176d49bb1126c7695
Tim <nigh.timothy@gmail.com>
2020-10-11 09:26:24 -0400

    added recursion to solution for day 2

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.IO;
 
 namespace Problem1
 {
     class Program
     {
+        const string FILE_PATH = "Day1In";
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] sInputs = File.ReadAllLines(FILE_PATH);
+            int[] nInputs = new int[sInputs.Length];
+            int total = 0;
+
+            //Convert to ints
+            for(int i = 0; i < sInputs.Length; i++)
+            {
+                if(int.TryParse(sInputs[i], out  int output))
+                {
+                    nInputs[i] = output;
+                    total += ComputeMass(nInputs[i]);
+                    
+                }
+                else
+                {
+                    Console.WriteLine($"Error at index {i}");
+                }
+
+            }
+
+            Console.WriteLine($"Total is: {total}");
+            Console.ReadKey();
+        }
+
+        private static int ComputeMass(int input)
+        { 
+            int fuel = input/3 -2;
+            if(fuel <= 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return fuel + ComputeMass(fuel);
+            }
         }
     }
 }
